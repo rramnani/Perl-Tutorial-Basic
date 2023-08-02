f19a0014e9201d4d74740a2ea4b227dd9c376097
rramnani <36605356+rramnani@users.noreply.github.com>
2023-08-02 12:14:43 -0500

    Create sigils.pl

---
+++ sigils.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# use strict 
+# use warnings
+
+# scalar variables use the sigil $
+my $num = 10; # scalar
+my $pi = 3.141596;
+my @names = ("John","Michael","Myers"); #array
+
+
+use Data::Dumper;
+#my %hash = ('abc' => 123, 'def' => [4,5,6]);
+my %dictionary1=("Rohit" , "Ramnani", "Meera" , "Ramnani", "Charu" , "Ramnani"); # dictionary or hash or key-value pair
+ 
+
+print ("hi");
+print ("\nValue of scalar num = ", $num, "pi = ", $pi);
+print ("\nValue of array names = ", join(", ", @names));
+print ("\nValue of dictionary1 names = ", Dumper(\%dictionary1));
+
