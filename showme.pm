efb6b414d8c42533ee9fff2e729f8fb015dc096c
rramnani <36605356+rramnani@users.noreply.github.com>
2023-08-03 11:11:26 -0500

    Create showme.pm

---
+++ showme.pm
@@ -0,0 +1,9 @@
+# user-defined modules
+package modules::showme;
+use strict; 
+use warnings;
+use 5.010;
+
+say "This is an user-defined module, Hi";
+
+1; # return true at the end or you can enter any string e.g., "I'm done!"
