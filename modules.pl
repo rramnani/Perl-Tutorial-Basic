a609148564f0d358431ff87fe07f02953e6c6b82
rramnani <36605356+rramnani@users.noreply.github.com>
2023-08-03 11:10:36 -0500

    Create modules.pl

---
+++ modules.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict; 
+use warnings;
+use 5.010;
+use modules::showme; # modules is the folder inside which showme.pm is present
+
+# user-defined modules
+
+
+
+# Perl modules example:
+
+#use Digest::MD5 'md5_hex';
+# functions from a vast library https://www.cpan.org/modules/index.html
+# reusable
+# over 108, 000 modules downloadable
+# http://learn.perl.org/modules/
+# some are built-in modules
+# say md5_hex("hello world");
+# say "\n", md5_hex("hello world1");
